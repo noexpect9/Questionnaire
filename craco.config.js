1701ce769b769b363f4b12a7c9b49fa52b005087
noexpect9 <1909293822@qq.com>
2024-03-28 21:01:42 +0800

    feat: build

--- craco.config.js
+++ craco.config.js
@@ -3,5 +3,34 @@ module.exports = {
     proxy: {
       '/api': 'http://localhost:8099/api'
     }
+  },
+  webpack: {
+    configure: (webpackConfig) => {
+      if(webpackConfig.mode === 'production') {
+        if(webpackConfig.optimization === null) {
+          webpackConfig.optimization = {}
+        }
+        webpackConfig.optimization.splitChunks = {
+          chunks: 'all',
+          cacheGroups: {
+            antd: {
+              name: 'antd-chunk',
+              test: /antd/,
+              priority: 100,
+            },
+            reactDom: {
+              name: 'react-dom-chunk',
+              test: /react-dom/,
+              priority: 99,
+            },
+            vendors: {
+              name: 'vendors-chunk',
+              test: /node_modules/,
+              priority: 98,
+            }
+          }
+        }
+      }
+    }
   }
 }
